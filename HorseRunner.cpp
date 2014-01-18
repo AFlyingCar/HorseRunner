2a297250d5f97ed60d4f15605a6b99a2177517aa
Tyler Robbins <tyler@familyrobbins.com>
2014-01-18 11:21:20 -0800

    First commit

---
+++ HorseRunner.cpp
@@ -0,0 +1,19 @@
+/****************\
+ *Tyler Robbins	*
+ *1/18/14	*
+ *Horse Runner  *
+\****************/
+
+#include <stdio.h>
+#include <Horse.h>
+
+using namespace std;
+
+int main(){
+
+	cout << "Test";
+
+	return(0);
+}
+
+char nuclear = '\u2622'
